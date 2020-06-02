8a084545dc6e40058b7920d4a1c6d3f176587fe5
DEMKOM58 <demkom58@gmail.com>
2020-06-02 12:25:22 +0300

    Refactor a bit

--- src/main/java/com/demkom58/nmlab6/regression/LsLinearRegression.java
+++ src/main/java/com/demkom58/nmlab6/regression/LsLinearRegression.java
@@ -19,6 +19,30 @@ public class LsLinearRegression implements Regression {
             ys[i] = xys[i].getD2();
         }
 
+        double correlation;
+        {
+            var mulSum = Arrays.stream(xys).mapToDouble(v -> v.getD1() * v.getD2()).sum();
+            var xSum = Arrays.stream(xs).sum();
+            var ySum = Arrays.stream(ys).sum();
+            var xySumMul = xSum * ySum;
+            var x2Sum = Arrays.stream(xs).map(x -> x * x).sum();
+            var y2Sum = Arrays.stream(ys).map(y -> y * y).sum();
+
+            correlation =
+                    (n * mulSum - xySumMul)
+                    /
+                    Math.sqrt((n * x2Sum - Math.pow(xSum, 2)) * (n * y2Sum - Math.pow(ySum, 2)));
+
+        }
+        double determination = Math.pow(correlation, 2);
+
+        DoubleUnaryOperator y = find(xs, ys);
+        return "Результат: " + y.applyAsDouble(searched) + "\n" +
+                "Індекс корреляції: " + correlation + "\n" +
+                "Індекс детермінації: " + determination;
+    }
+
+    private DoubleUnaryOperator find(double[] xs, double[] ys) {
         var xMid = Arrays.stream(xs).average().orElse(Double.NaN);
         var yMid = Arrays.stream(ys).average().orElse(Double.NaN);
 
@@ -38,27 +62,7 @@ public class LsLinearRegression implements Regression {
         var slope = xy / xx;
         var intercept = yMid - slope * xMid;
 
-        double correlation = 0;
-        {
-            var mulSum = Arrays.stream(xys).mapToDouble(v -> v.getD1() * v.getD2()).sum();
-            var xSum = Arrays.stream(xs).sum();
-            var ySum = Arrays.stream(ys).sum();
-            var xySumMul = xSum * ySum;
-            var x2Sum = Arrays.stream(xs).map(x -> x * x).sum();
-            var y2Sum = Arrays.stream(ys).map(y -> y * y).sum();
-
-            correlation =
-                    (n * mulSum - xySumMul)
-                    /
-                    Math.sqrt((n * x2Sum - Math.pow(xSum, 2)) * (n * y2Sum - Math.pow(ySum, 2)));
-
-        }
-        double determination = Math.pow(correlation, 2);
-
-        DoubleUnaryOperator y = (double x) -> slope * x + intercept;
-        return "Результат: " + y.applyAsDouble(searched) + "\n" +
-                "Індекс корреляції: " + correlation + "\n" +
-                "Індекс детермінації: " + determination;
+        return (double x) -> slope * x + intercept;
     }
 
 }
